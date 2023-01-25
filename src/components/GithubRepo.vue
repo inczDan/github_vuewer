cd411aa3ec2dbc64559bb6ce81fcbd2f12a29d46
daniel.matos <daniel.matos@buser.com.br>
2023-01-25 11:00:04 -0300

    add listagem de repositorios

--- src/components/GithubRepo.vue
+++ src/components/GithubRepo.vue
@@ -1,12 +1,30 @@
 <template>
     <div>
-        <v-autocomplete
-        v-model="user"
-        :items="userlist"
-        :loading="userloading"
-        :search-input.sync="usersearch"
-        item-text="login"
-        />
+        <v-row class="text-center">
+            <v-col cols="6">
+                <v-autocomplete
+                v-model="user"
+                label="Procure um user"
+                :items="userlist"
+                :loading="userloading"
+                :search-input.sync="usersearch"
+                item-text="login"
+                />
+            </v-col>
+            <v-col cols="6">
+                <v-select
+                    v-model="repo"
+                    :items="repolist"
+                    :loading="repoloading"
+                    item-text="name"
+                    label="Selecione o repositorio desejado"
+                    return-object
+                    single-line
+                ></v-select>
+            </v-col>
+        </v-row>
+       
+        
     </div>
 </template>
   
@@ -19,21 +37,38 @@
     export default {
         data: () => ({
             user: null,
+            repo: null,
             usersearch: null,
             userlist: [],
-            userloading: false
+            repolist: [],
+            userloading: false,
+            repoloading: false,
         }),
         methods: {
             procuraUsuariosGithub: debouncerdecorator(async function() {
-                 this.userloading = true
-                 const data = await api.search_users(this.usersearch)
-                    this.userlist = data.items
-                    this.userloading = false
+                this.userloading = true
+                const data = await api.search_users(this.usersearch)
+                this.userlist = data.items
+                this.userloading = false
             },500),
+            async listaRepositorios(){
+                this.repoloading= true
+                const data = await api.lista_repos(this.user)
+                this.repolist = data
+                this.repoloading= false
+            }
         },
         watch: {
             usersearch (){
                 this.procuraUsuariosGithub()
+            },
+            user(){
+                this.listaRepositorios = [
+,
+                ]
+            },
+            repo (){
+
             }
         }
     }
